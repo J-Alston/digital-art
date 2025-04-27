b62602704206a666765a4b404456178e394a7672
J-Alston <jackalston3@hotmail.co.uk>
2025-04-27 12:08:24 +0100

    turn line into object to access key variable

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,20 +1,28 @@
 import numpy as np
 
-def short_line(x,y,l,theta):
-    '''
-    Defines a line object centred on coords (x,y), with length l, and at an angle theta.
-    '''
-    x1 = x + l/2*np.cos(theta)
-    y1 = y + l/2*np.sin(theta)
-    x2 = x - l/2*np.cos(theta)
-    y2 = y - l/2*np.sin(theta)
+class short_line:
+    def __init__(self, x, y, l, theta):
+        '''
+        Defines a line object centred on coords (x,y), with length l, and at an angle theta.
+        '''
+        self.x = x
+        self.y = y
+        self.l = l
+        self.theta = theta
 
-    xy = [(x1, y1), (x2, y2)]
+        x1 = self.x + self.l/2*np.cos(self.theta)
+        y1 = self.y + self.l/2*np.sin(self.theta)
+        x2 = self.x - self.l/2*np.cos(self.theta)
+        y2 = self.y - self.l/2*np.sin(self.theta)
 
-    return xy
+        self.xy = [(x1, y1), (x2, y2)]
 
 def line_grid(theta_field, l_field):
     '''
     Draws a grid of lines with angles and lengths corresponding to fields provided.
     theta_field and l_field are equally sized 2-d arrays. 
     '''
+
+    lg = short_line(theta_field, l_field)
+
+    return lg
