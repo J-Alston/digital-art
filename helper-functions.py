3e1cefe8c9b81f8cf0ede731b4c7c88fd3fc9205
J-Alston <jackalston3@hotmail.co.uk>
2025-04-27 11:15:24 +0100

    Added some function framework

--- helper-functions.py
+++ helper-functions.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+def short_line(x,y,l,theta):
+    '''
+    Defines a line object centred on coords (x,y), with length l, and at an angle theta.
+    '''
+
+def line_grid(theta_field, l_field):
+    '''
+    Draws a grid of lines with angles and lengths corresponding to fields provided.
+    theta_field and l_field are equally sized 2-d arrays. 
+    '''
