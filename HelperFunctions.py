fa61f5311396c164eec0c157d1d5e4d477f337cc
J-Alston <jackalston3@hotmail.co.uk>
2025-04-27 11:32:31 +0100

    TRIGANOMETRY!!!!!

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -4,6 +4,14 @@ def short_line(x,y,l,theta):
     '''
     Defines a line object centred on coords (x,y), with length l, and at an angle theta.
     '''
+    x1 = x + l/2*np.cos(theta)
+    y1 = y + l/2*np.sin(theta)
+    x2 = x - l/2*np.cos(theta)
+    y2 = y - l/2*np.sin(theta)
+
+    xy = [(x1, y1), (x2, y2)]
+
+    return xy
 
 def line_grid(theta_field, l_field):
     '''
